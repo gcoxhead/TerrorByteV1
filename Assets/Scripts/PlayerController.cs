27f3e4666366c4957c33aa5e334a45d570bb9352
gcoxhead <79542013+gcoxhead@users.noreply.github.com>
2022-07-19 19:31:07 +0100

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    //private variables. Make public to display in the unity inspector
+    [SerializeField] private float speed = 10.0f;
+    [SerializeField] private float horsePower = 0;
+    [SerializeField] private float turnSpeed = 90.0f;
+    private float horizontalInput;
+    private float verticalInput;
+    private Rigidbody playerRb;
+    //[SerializeField] GameObject centerOfMass;
+
+    //Start is called once on start up
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        //playerRb.centerOfMass = centerOfMass.transform.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //Get player input from controls
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        
+        //Move the forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput) ;
+        //playerRb.AddRelativeForce(Vector3.forward * horsePower * verticalInput);
+
+        //Move the vehicle in the horizontal axis
+        //transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput) ;
+
+        //Rotate the character
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput) ;
+    }
+}
---
+++ Assets/animationStateController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animationStateController : MonoBehaviour
+{
+    Animator animator;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        Debug.Log(animator);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("up"))
+            {
+                animator.SetBool("isRunning", true);
+            }
+        if (!Input.GetKey("up"))
+            {
+                animator.SetBool("isRunning", false);
+            }
+
+        if (Input.GetKey("space"))
+            {
+                animator.SetBool("isJumping", true);
+            } 
+
+         if (!Input.GetKey("space"))
+            {
+                animator.SetBool("isJumping", true);
+            }   
+    }
+}
+
